20dc9e22c62c94836e83c407d13205cdafd485e6
PhenomenalMatrix <mrflaitx@gmail.com>
2020-04-27 05:49:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/game/BlankFragment.java
@@ -0,0 +1,95 @@
+package com.example.game;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BlankFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment extends Fragment {
+
+     TextView textView;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private Integer mParam1;
+//    private String mParam2;
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView = view.findViewById(R.id.fragmentText);
+        Random random = new Random();
+        int randomIndex = random.nextInt((100) + 1);
+        Log.d("ololo", "otvet: "+ randomIndex);
+        if (randomIndex == mParam1){
+            textView.setText("Угадал");
+        }
+        else {
+            textView.setText("Не угадал");
+        }
+
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BlankFragment newInstance(Integer param1) {
+        BlankFragment fragment = new BlankFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getInt(ARG_PARAM1);
+            randomizer(mParam1);
+            Log.d("olo", "From fragment");
+            Log.d("olo",""+ mParam1);
+        }
+    }
+
+    private void randomizer(int s) {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return
+                inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+private TextView textView;
+private SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.progress_bar);
+        seekBar = findViewById(R.id.bar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText("" + progress );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                showResultsFragment();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    private  void  showResultsFragment(){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        Integer in ;
+        BlankFragment fragment = BlankFragment.newInstance(Integer.valueOf(textView.getText().toString()));
+        transaction.replace(R.id.frame_layout_text,fragment);
+        transaction.commit();
+    }
+
+
+
+
+}
